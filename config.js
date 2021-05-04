8fc6ac3f8bd2efa30ef9d67e5fcfe5cd1f6208eb
Tapan Patel <tapan2930@gmail.com>
2021-05-04 14:41:29 +0530

    v2

--- config.js
+++ config.js
@@ -1,16 +1,16 @@
-export const cowinInfo = {
+exports.cowinInfo = {
     areaCode : "390020",
     url:"https://www.cowin.gov.in/home",
     cat:"18-44", // 45+ or 18-45
 }
 
-export const emailInfo = {
+exports.emailInfo = {
     from:"itstap15@gmail.com",
     to:"tapan2930@gmail.com",
     g_user:process.env.USER,
     g_pass:process.env.PASS,
 }
 
-export const appInfo = {
+exports.appInfo = {
     interval: 900000
 }
